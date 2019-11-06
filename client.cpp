c20d0c73c4e5600e616ca8d6ba62889dfb00fff8
Ryan Wickman <rwickman@memphis.edu>
2019-11-05 20:27:47 -0600

    add new comments and change client to use iostream

--- client.cpp
+++ client.cpp
@@ -1,6 +1,7 @@
 
 // Client side C/C++ program to demonstrate Socket programming 
-#include <stdio.h> 
+#include <iostream>
+//#include <stdio.h> 
 #include <sys/socket.h> 
 #include <arpa/inet.h> 
 #include <unistd.h> 
@@ -9,35 +10,35 @@
    
 int main(int argc, char const *argv[]) 
 { 
-    int sock = 0, valread; 
-    struct sockaddr_in serv_addr; 
-    char *hello = "Hello from client"; 
-    char buffer[1024] = {0}; 
+    int sock = 0, valread;
+    struct sockaddr_in serv_addr;
+    char buffer[1024] = {0};
+    std::string hello = "Hello from client";
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
     { 
-        printf("\n Socket creation error \n"); 
-        return -1; 
-    } 
-   
+	std::cout << "\n Socket creation error" << std::endl; 
+        return -1;
+    }
+
     serv_addr.sin_family = AF_INET; 
     serv_addr.sin_port = htons(PORT); 
-       
-    // Convert IPv4 and IPv6 addresses from text to binary form 
-    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)  
-    { 
-        printf("\nInvalid address/ Address not supported \n"); 
-        return -1; 
-    } 
-   
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
-    { 
-        printf("\nConnection Failed \n"); 
-        return -1; 
-    } 
-    send(sock , hello , strlen(hello) , 0 ); 
-    printf("Hello message sent\n"); 
-    valread = read( sock , buffer, 1024); 
-    printf("%s\n",buffer ); 
-    return 0; 
+
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)
+    {
+       std::cout << "\nInvalid address/ Address not supported" << std::endl;
+        return -1;
+    }
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        std::cout << "\nConnection Failed \n" << std::endl;
+        return -1;
+    }
+    send(sock, hello.c_str() , hello.length() , 0);
+    std::cout << "Hello message sent\n" << std::endl;
+    valread = read( sock , buffer, 1024);
+    std::cout << buffer << std::endl;
+    return 0;
 } 
 
--- server.cpp
+++ server.cpp
@@ -3,29 +3,27 @@
 //#include <stdio.h> 
 #include <unistd.h> 
 #include <sys/socket.h> 
-#include <stdlib.h> 
-#include <netinet/in.h> 
+#include <netinet/in.h> // constants and structures needed for internet domain addresses 
 #include <string.h> 
 
 #define PORT 8080 
 
 int main(int argc, char const *argv[]) 
 { 
-    int server_fd, new_socket, valread; 
-    struct sockaddr_in address; 
+    int server_fd, new_socket_fd, valread; 
+    struct sockaddr_in address; // Contains an internet address 
     int opt = 1; 
     int addrlen = sizeof(address); 
     char buffer[1024] = {0}; 
     std::string hello = "Hello from server"; 
        
-    // Creating socket file descriptor 
+    // Creating socket file descriptor using IPV4 and TCP 
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
     { 
 	std::cerr << "socket failed" << std::endl; 
         exit(EXIT_FAILURE); 
     } 
        
-    // Forcefully attaching socket to the port 8080 
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, 
                                                   &opt, sizeof(opt))) 
     { 
@@ -33,30 +31,31 @@ int main(int argc, char const *argv[])
         exit(EXIT_FAILURE); 
     } 
     address.sin_family = AF_INET; 
-    address.sin_addr.s_addr = INADDR_ANY; 
-    address.sin_port = htons( PORT ); 
+    address.sin_addr.s_addr = INADDR_ANY; // Set the IP address 
+    address.sin_port = htons(PORT); // convert PORT from host byte order to network byte order
        
-    // Forcefully attaching socket to the port 8080 
-    if (bind(server_fd, (struct sockaddr *)&address,  
-                                 sizeof(address))<0) 
+    // Bind the socket to the address
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) 
     { 
 	std::cerr << "bind failed" << std::endl; 
         exit(EXIT_FAILURE); 
-    } 
-    if (listen(server_fd, 3) < 0) 
+    }
+
+    // Listen on this server for connections and allow for a backlog of 5
+    if (listen(server_fd, 5) < 0) 
     { 
 	std::cerr << "listen" << std::endl; 
         exit(EXIT_FAILURE); 
     } 
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,  
+    if ((new_socket_fd = accept(server_fd, (struct sockaddr *)&address,  
                        (socklen_t*)&addrlen))<0) 
     { 
 	std::cerr << "accept" << std::endl;
         exit(EXIT_FAILURE); 
     }
 
-    valread = read( new_socket , buffer, 1024);
+    valread = read(new_socket_fd, buffer, 1024);
     std::cout << buffer << std::endl;
-    send(new_socket , hello.c_str() , hello.size() , 0);
+    send(new_socket_fd, hello.c_str() , hello.size() , 0);
     std::cout << "Hello message sent" << std::endl;
 }
