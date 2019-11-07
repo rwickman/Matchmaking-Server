e8db21e757b406fb905b9924708246bd045e3151
Ryan Wickman <rwickman@memphis.edu>
2019-11-06 20:53:38 -0600

    Set up cmake

---
+++ build/CMakeFiles/Matchmaking-Server.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Matchmaking-Server.dir/server_asio.cpp.o"
+  "Matchmaking-Server.pdb"
+  "Matchmaking-Server"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Matchmaking-Server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
