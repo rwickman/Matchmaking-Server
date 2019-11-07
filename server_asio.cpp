b52ffdacb6ae55f30619042becd7d6d5e199aab2
Ryan Wickman <rwickman@memphis.edu>
2019-11-07 09:44:14 -0600

    Begin working on asio tutorial

--- server_asio.cpp
+++ server_asio.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 #include <boost/asio.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+void print(const boost::system::error_code& /*e*/)
+{
+  std::cout << "Hello, world!" << std::endl;
+}
 
 int main() {
-  std::cout << "Hello World!" << std::endl;
+  boost::asio::io_context io;
+  
+  boost::asio::deadline_timer timer(io, boost::posix_time::seconds(10));
+  timer.async_wait(&print);
+  io.run();
+  
+  return 0;
 }
