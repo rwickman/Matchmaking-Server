b351e71d12c4cf071953fb93919231627ba8b827
Ryan Wickman <rwickman@memphis.edu>
2019-11-16 18:27:40 -0600

    Add JoinPacket for client join game request

---
+++ server/join_packet.cpp
@@ -0,0 +1,34 @@
+#include "join_packet.hpp"
+
+namespace Matchmaking
+{
+  JoinPacket::JoinPacket()
+  {
+  }
+
+  std::string JoinPacket::get_user_id()
+  {
+    return user_id;
+  }
+
+  int JoinPacket::get_game_type()
+  {
+    return game_type;
+  }
+
+  bool JoinPacket::decode_join()
+  {
+    try
+    {
+      nlohmann::json join_json = nlohmann::json::parse(body());
+      user_id = join_json["User ID"].get<std::string>();
+      game_type = static_cast<GameType>(join_json["Game Type"].get<int>());
+      return true;
+    }
+    catch(std::exception& e)
+    {
+      std::cerr << e.what() << std::endl;
+      return false;
+    }
+  }
+}
---
+++ server/join_packet.hpp
@@ -0,0 +1,39 @@
+#ifndef MATCHMAKING_JOIN_PACKET_HPP
+#define MATCHMAKING_JOIN_PACKET_HPP
+
+
+#include <string>
+
+#include "packet.hpp"
+#include <nlohmann/json.hpp>
+
+namespace Matchmaking
+{
+
+class JoinPacket : Packet
+{
+public:
+  enum { header_length = 8 };
+  enum { max_body_length = 512 };
+  enum GameType {
+    ELIMINATION = 0,
+  };
+
+  JoinPacket();
+
+  std::string get_user_id();
+
+  int get_game_type();
+
+  bool decode_join();
+
+protected:
+  // In the future user identification may need its own class
+  std::string user_id;
+  GameType game_type;
+
+};
+
+}
+
+#endif // MATCHMAKING_JOIN_PACKET_HPP
